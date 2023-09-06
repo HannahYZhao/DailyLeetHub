0dba24e6773f8e8f417ab2fffd726e9832b92012
HannahYZhao <47459029+HannahYZhao@users.noreply.github.com>
2023-09-06 17:27:55 +1000

    Time: 426 ms (73.84%), Space: 29 MB (56.52%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,15 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hset = set()
+        for idx in nums:
+            if idx in hset:
+                return True
+            else:
+                hset.add(idx)
+        
