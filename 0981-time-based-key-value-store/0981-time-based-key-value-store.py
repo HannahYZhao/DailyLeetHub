7fcc3eb3d3a43448b8fc8fd4e5b77d76c28a1a7d
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-10-09 07:18:00 +1100

    Time: 545 ms (97.41%), Space: 70.7 MB (97.75%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -0,0 +1,23 @@
+# Time complexity: O(log n). set: O(1), get: O(log k) the worst case where k == n
+# Space complexity: O(n). 
+class TimeMap:
+
+    def __init__(self):
+        self.meta = collections.defaultdict(list)
+        self.data = collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.meta[key].append(timestamp)
+        self.data[key].append(value)
+
+    def get(self, key: str, timestamp: int) -> str:
+        idx = bisect.bisect(self.meta[key], timestamp)
+        if idx == 0:
+            return ''
+        return self.data[key][idx - 1]
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
