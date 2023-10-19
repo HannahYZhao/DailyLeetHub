ef1deb32ebb78c303cf0b62ed49a74f4c2238534
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-10-20 09:16:21 +1100

    Time: 40 ms (69.20%), Space: 16.4 MB (21.47%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,36 +1,42 @@
-# Time complexity: O(log(x))
-# Space complexity: O(1)
-class Solution(object):
-    def myAtoi(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # Remove leading and trainling whitespace
-        """
-        example of strip syntx in python:
-        txt = ",,,,,rrttgg.....banana....rrr"
-        x = txt.strip(",.grt")
-        print(x)
-        result:banana
-        """
-        s = s.strip( )
-        if not s:
-            # Handle empty string case
+# DFA solution
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        value, state, pos, sign = 0, 0, 0, 1
+
+        if len(str) == 0:
             return 0
-        num = 0
-        i = 0
-        # 1 for postive, -1 for negative
-        sign = 1
-        if s[i] == '+':
-            i += 1
-        elif s[i] == '-':
-            i += 1
-            sign = -1
-        while i < len(s) and s[i].isdigit():
-            num = num * 10 + int(s[i])
-            i += 1
-        num *= sign
-        # Check for integer overflow
-        num = max(min(num, 2 ** 31 - 1), -2 ** 31)
-        return num
+
+        while pos < len(str):
+            current_char = str[pos]
+            if state == 0:
+                if current_char == " ":
+                    state = 0
+                elif current_char == "+" or current_char == "-":
+                    state = 1
+                    sign = 1 if current_char == "+" else -1
+                elif current_char.isdigit():
+                    state = 2
+                    value = value * 10 + int(current_char)
+                else:
+                    return 0
+            elif state == 1:
+                if current_char.isdigit():
+                    state = 2
+                    value = value * 10 + int(current_char)
+                else:
+                    return 0
+            elif state == 2:
+                if current_char.isdigit():
+                    state = 2
+                    value = value * 10 + int(current_char)
+                else:
+                    break
+            else:
+                return 0
+            pos += 1
+
+        value = sign * value
+        value = min(value, 2 ** 31 - 1)
+        value = max(-(2 ** 31), value)
+
+        return value
