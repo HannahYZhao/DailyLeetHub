d2a214e482b377bc361d97d88a2fbf7125a0ffac
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-10-22 08:51:18 +1100

    Time: 16 ms (46.83%), Space: 13.1 MB (73.46%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,39 +1,22 @@
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix:
-            return []
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        return matrix and list(matrix.pop(0)) + self.spiralOrder(zip(*matrix)[::-1])
+"""
+Python3:
+def spiralOrder(self, matrix):
+    return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
 
-        rows, cols = len(matrix), len(matrix[0])
-        top, bottom, left, right = 0, rows - 1, 0, cols - 1
-        result = []
-        # To traverse each layer, we need to keep track of the four boundaries of the current spiral
-        while len(result) < rows * cols:
-            for i in range(left, right + 1):
-                # Add the elements to the result list
-                """
-                The append() function in Python takes a single item as an input parameter and adds it to the end of the given list. In Python, append() doesn't return a new list of items; in fact, it returns no value at all.
-                
-                fruits = ["apple", "banana", "cherry"]
-                fruits.append("orange")
-                print(fruits)
-                """
-                result.append(matrix[top][i])
-            # From left move to the right
-            top += 1
+The difference betweem Python and Python3
 
-            for i in range(top, bottom + 1):
-                result.append(matrix[i][right])
-            # From right move to the left
-            right -= 1
+Python3 > Python
+to improve its consistency and eliminate some of the issues in Python 2. 
+Use Python3 for new project
+zip() Function:
 
-            if top <= bottom:
-                for i in range(right, left - 1, -1):
-                    result.append(matrix[bottom][i])
-                bottom -= 1
-            
-            if left <= right:
-                for i in range(bottom, top - 1,  -1):
-                    result.append(matrix[i][left])
-                left += 1
-            
-        return result
+Python 2: Returned a list.
+Python 3: Returns an iterator.
+"""
