22f84adb7093f90ac839b25171b76226f88177e0
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-11-04 09:21:40 +1100

    Time: 38 ms (57.54%), Space: 16.4 MB (26.49%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,14 +1,11 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        # Check if the input 'digits' is an empty string
-        if '' == digits: 
+# Time complexity: O(4^n)
+# Space complexity: O(n)
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
             return []
-        # Define a dictionary that maps each digit to its associated letters
-        kvmaps = {
+
+        phone_map = {
             '2': 'abc',
             '3': 'def',
             '4': 'ghi',
@@ -18,8 +15,14 @@ class Solution(object):
             '8': 'tuv',
             '9': 'wxyz'
         }
-        # Use the 'reduce' function to compute the letter combinations
-        '''
-        It applies a lambda function to each digit in the input string 'digits', starting with an initial value of ['']. The lambda function takes two arguments: acc (the accumulated result so far) and digit (the current digit being processed).
-        '''
-        return reduce(lambda acc, digit: [x + y for x in acc for y in kvmaps[digit]], digits, [''])
+
+        def backtrack(combination, next_digits):
+            if len(next_digits) == 0:
+                output.append(combination)
+            else:
+                for letter in phone_map[next_digits[0]]:
+                    backtrack(combination + letter, next_digits[1:])
+
+        output = []
+        backtrack("", digits)
+        return output        
