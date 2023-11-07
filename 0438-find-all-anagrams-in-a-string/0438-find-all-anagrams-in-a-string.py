98c10b4a9f012e7aaab6d1c01d72e2bb1ad39a8b
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-11-08 07:44:41 +1100

    Time: 89 ms (50.55%), Space: 14.4 MB (54.63%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,37 +1,33 @@
-# Time complextity: O(n)
-# Space complexity: O(n)
-class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        window = len(p)
-        mapper = {}
-        def __mapper():
-            nonlocal mapper
-            for i in p:
-                if i in mapper.keys(): mapper[i] += 1
-                else: mapper[i] = 1
+# Time complexity: O(n)
+# Space complexity: O(1)
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        # hm is a dictionary (hashmap) that is used to keep track of the character frequencies in the pattern string p. For each character in p, its frequency is stored in the dictionary. pL and sL represent the lengths of strings p and s, respectively.
 
-        __mapper()
-        ans = []
-        st = 0
-        ed = 0
-        while ed < len(s):
-            if s[ed] in mapper.keys():
-                #-= 1 is the shorthand notation for subtracting 1 from the current value. In other words, it decrements the count of the character s[ed] in the mapper dictionary. It's a concise way of expressing mapper[s[ed]] = mapper[s[ed]] - 1.
-                mapper[s[ed]] -= 1         
-                if mapper[s[ed]] == 0:
-                    del mapper[s[ed]]
-                if not bool(mapper):
-                    ans.append(st)
-                    mapper[s[st]] = 1
-                    st += 1
-                ed += 1
-            else:
-                if st == ed:
-                    st = ed = ed + 1
-                else:
-                    if s[st] in mapper.keys():
-                        mapper[s[st]] += 1
-                    else:
-                        mapper[s[st]] = 1
-                    st += 1
-        return ans
+        hm, res, pL, sL = defaultdict(int), [], len(p),len(s)
+        # The code first checks if the length of p is greater than the length of s. If this is the case, there can be no anagrams in s, so it returns an empty list [].
+        if pL > sL: return []
+
+        # Build hashmap
+        for ch in p: hm[ch] += 1
+
+        # Initial full pass over the window
+        for i in range(pL-1):
+            if s[i] in hm: hm[s[i]] -= 1
+
+        # Slide the window with stride 1
+        for i in range(-1, sL-pL+1):
+            if i > -1 and s[i] in hm:
+                hm[s[i]] += 1
+            if i+pL < sL and s[i+pL] in hm:
+                hm[s[i+pL]] -= 1
+
+                # Check whether we encountered an anagram
+                if all(v == 0 for v in hm.values()):
+                    res.append(i+1)
+        return res
