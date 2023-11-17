2270136eb1cdf43628c52a09158eed1368bb2c0d
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-11-18 10:13:31 +1100

    Time: 57 ms (33.78%), Space: 20.3 MB (90.85%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,28 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# BST
-class Solution(object):
-    # k, the position of the smallest element we want to find
-    def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-        count = []
-        # Calls the helper method to perform an in-order traversal of the tree. This traversal visits the nodes of the BST in ascending order.
-        self.helper(root, count)
-        return count[k-1]
-    # Recursively calls the helper method on the left child of the current node. This explores all the nodes in the left subtree (which are smaller than the current node).
-    def helper(self, node, count):
-        if not node:
-            return
-        
-        self.helper(node.left, count)
-        # Adds the value of the current node to the count list. In an in-order traversal, this step occurs after visiting the left subtree and before visiting the right subtree.
-        count.append(node.val)
-        self.helper(node.right, count)
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
