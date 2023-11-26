a4daf5f6aad81632e091becebb3d51863a69be07
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-11-26 11:04:30 +1100

    Time: 155 ms (11.07%), Space: 22.3 MB (58.03%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -6,42 +6,41 @@
 #         self.right = None
 
 class Codec:
+#    def serialize(self, root):
+#            if not root: return 'x'
+#            return root.val, self.serialize(root.left), self.serialize(root.right)
 
-    def serialize(self, root):
-        """Encodes a tree to a single string.
-        
-        :type root: TreeNode
-        :rtype: str
-        """
-        def doit(node):
-            if node:
-                vals.append(str(node.val))
-                doit(node.left)
-                doit(node.right)
-            else:
-                vals.append('#')
-        vals = []
-        doit(root)
-        return ' '.join(vals)
-
-    def deserialize(self, data):
-        """Decodes your encoded data to tree.
-        
-        :type data: str
-        :rtype: TreeNode
-        """
-        def doit():
-            val = next(vals)
-            if val == '#':
-                return None
-            node = TreeNode(int(val))
-            node.left = doit()
-            node.right = doit()
-            return node
-        vals = iter(data.split())
-        return doit()
-
+#    def deserialize(self, data):
+#        if data[0] == 'x': return None
+#        node = TreeNode(data[0])
+#        node.left = self.deserialize(data[1])
+#        node.right = self.deserialize(data[2])
+#        return node
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+# The issue with your serialize method is that you are returning a tuple instead of a string. The serialized data should be a string, not a tuple. To fix this, you can convert the tuple to a string using a separator like a comma. Here's the corrected code:
+
+    def serialize(self, root):
+        if not root:
+            return 'x'
+        left_serialized = self.serialize(root.left)
+        right_serialized = self.serialize(root.right)
+        return f"{root.val},{left_serialized},{right_serialized}"
+
+    def deserialize(self, data):
+        nodes = data.split(',')
+        return self.deserialize_helper(nodes)
+
+    def deserialize_helper(self, nodes):
+        if not nodes:
+            return None
+        val = nodes.pop(0)
+        if val == 'x':
+            return None
+        node = TreeNode(int(val))
+        node.left = self.deserialize_helper(nodes)
+        node.right = self.deserialize_helper(nodes)
+        return node
