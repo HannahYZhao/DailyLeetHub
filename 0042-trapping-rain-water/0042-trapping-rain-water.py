707c583976572b835c51c17fa745b0b59f542b3a
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-11-28 21:39:30 +1100

    Time: 98 ms (97.65%), Space: 18.2 MB (45.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,16 +1,36 @@
+# Time complexity: O(n)
+# Space complexity: O(1)
+
 class Solution:
-    def trap(self, bars):
-        if not bars or len(bars) < 3:
+    def trap(self, height: List[int]) -> int:
+        
+        if len(height) <= 2:
             return 0
-        volume = 0
-        left, right = 0, len(bars) - 1
-        l_max, r_max = bars[left], bars[right]
-        while left < right:
-            l_max, r_max = max(bars[left], l_max), max(bars[right], r_max)
-            if l_max <= r_max:
-                volume += l_max - bars[left]
-                left += 1
+        ans = 0
+
+        # Using two pointers i and j on indices 1 and n - 1
+        i = 1
+        j = len(height) - 1
+
+        # Initialising leftmax to the leftmost bar and rightmax to the rightmost bar
+        lmax = height[0]
+        rmax = height[-1]
+
+        while i <= j:
+            # Check lmax and rmax for current i, j positions
+            if height[i] > lmax:
+                lmax = height[i]
+            if height[j] > rmax:
+                rmax = height[j]
+
+            # Fill water upto lmax level for index i and move i to the right
+            if lmax <= rmax:
+                ans += lmax - height[i]
+                i += 1
+
+            # Fill water upto rmax level for index j and move j to the left
             else:
-                volume += r_max - bars[right]
-                right -= 1
-        return volume
+                ans += rmax - height[j]
+                j -= 1
+        
+        return ans
