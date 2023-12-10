378d25ab1af6d3a2796afa40573c0442d0b38e5c
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-12-10 12:03:08 +1100

    Time: 754 ms (22.07%), Space: 125 MB (6.47%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,20 +1,15 @@
-class Solution(object):
-    def jobScheduling(self, startTime, endTime, profit):
-        """
-        :type startTime: List[int]
-        :type endTime: List[int]
-        :type profit: List[int]
-        :rtype: int
-        """
-        # sorted(..., key=lambda v: v[1]): This part takes the list of tuples created in step 1 and sorts them based on the second element of each tuple, which is the end time (endTime). The key parameter specifies a function (lambda v: v[1]) that extracts the end time from each tuple for the sorting comparison. This means that the list of jobs will be sorted in ascending order of their end times.
+# Time complexity: O(N*logN)
+# Space complexity: O(N) 
+class Solution:
+    def jobScheduling(self, s: List[int], e: List[int], p: List[int]) -> int:
+        
+        jobs = sorted(zip(s, e, p))
+        
+        @lru_cache(None)
+        def dfs(i):
+            if i >= len(jobs) : return 0
+            k = bisect_left(jobs, jobs[i][1], key = lambda j: j[0])
+            return max(dfs(i+1), jobs[i][2] + dfs(k))
+            
+        return dfs(0)
 
-
-        jobs = sorted(zip(startTime, endTime, profit), key = lambda v: v[1])
-        # Intial dp[0] = 0, as we make profit = 0 at time = 0.
-        dp = [[0, 0]]
-        # s, e, p are its start time, end time and profit
-        for s, e, p ,in jobs:
-            i = bisect.bisect(dp, [s + 1]) - 1
-            if dp[i][1] + p > dp[-1][1]:
-                dp.append([e, dp[i][1] + p])
-        return dp[-1][1]
