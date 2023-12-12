6ee274eccaaad53668284fa3c2353a73730902be
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-12-12 16:22:34 +1100

    Time: 88 ms (85.09%), Space: 20.4 MB (29.36%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,25 +1,71 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# This line imports the PriorityQueue data structure from the Python Queue module. PriorityQueue is a type of queue where elements are dequeued based on their priority.
-from Queue import PriorityQueue
-class Solution(object):
+class Solution:
     def mergeKLists(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
-        dummy = ListNode(None)
+        heap = [(head.val, i, head) for i,head in enumerate(lists) if head]
+        heapify(heap)
+        dummy = ListNode(0)
         curr = dummy
-        q = PriorityQueue()
-        for node in lists:
-            if node: q.put((node.val, node))
-        while q.qsize() > 0:
-            curr.next = q.get()[1]
+        while heap != []:
+            val, i, node = heap[0]
+            if not node.next: # exhausted one linked-list
+                heappop(heap)
+            else:
+                heapreplace(heap, (node.next.val, i, node.next)) # recycling tie-breaker i guarantees uniqueness
+            curr.next = node    
             curr = curr.next
-            if curr.next: q.put((curr.next.val, curr.next))
         return dummy.next
-     
-    
+
+
+'''
+def mergeKLists(self, lists):
+    from heapq import heappush, heappop, heapreplace, heapify
+    dummy = node = ListNode(0)
+    h = [(n.val, n) for n in lists if n]
+    heapify(h)
+    while h:
+        v, n = h[0]
+        if n.next is None:
+            heappop(h)  # Only change heap size when necessary
+        else:
+            heapreplace(h, (n.next.val, n.next))
+        node.next = n
+        node = node.next
+
+    return dummy.next
+Runtime error:
+Error message:
+TypeError: '<' not supported between instances of 'ListNode' and 'ListNode'
+    heapify(h)
+Line 11 in mergeKLists (Solution.py)
+    ret = Solution().mergeKLists(param_1)
+Line 41 in _driver (Solution.py)
+    _driver()
+Line 52 in <module> (Solution.py)
+
+I see the issue now. The error is occurring because you're trying to use the heapify function on a list of tuples containing (n.val, n) pairs, where n is a ListNode object. The heapify function expects the elements of the list to be directly comparable, and ListNode objects aren't inherently comparable.
+
+To fix this, you can modify the code to use a custom comparison key for the heapq functions. Here's the updated code:
+
+Fixed code:
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists):
+        dummy = node = ListNode(0)
+        h = [(n.val, n) for n in lists if n]
+        heapq.heapify(h)  # Use heapq
+        while h:
+            val, n = heapq.heappop(h)
+            if n.next is not None:
+                heapq.heappush(h, (n.next.val, n.next))
+            node.next = n
+            node = node.next
+
+        return dummy.next
+
+By using heapq and specifying the (n.val, n) tuples as the elements to be compared, you can correctly perform the heap operations on the list of linked lists. This code should work without errors in Python 3.
+'''
