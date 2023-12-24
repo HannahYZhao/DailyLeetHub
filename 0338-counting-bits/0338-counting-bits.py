ebb947bee8bb7caca1465e9b6e17a79a7a80467b
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2023-12-24 16:32:06 +1100

    Time: 56 ms (98.90%), Space: 24.2 MB (5.28%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,18 +1,11 @@
-class Solution(object):
-    def countBits(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
-        # Define the base case, and start at 1
-        counter = [0]
-        # We need to include n, so use n+1 as the bound on the range
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        result = [0] * (n+1)
+        offset = 1
         for i in range(1, n+1):
-            # Bit-shifting 1 has the same effect as dividing by 2, and is faster, so replace i // 2 with i >> 1
-            '''
-            Left Shift (<<):0010 << 1 becomes 0100, which is 2 left-shifted by 1 bit to become 4.
-            Right Shift (>>): in binary, 0100 >> 1 becomes 0010, which is 4 right-shifted by 1 bit to become 2.
-            In many cases, bit-shifting can be faster than division or multiplication because it is a simpler operation at the hardware level. However, in high-level languages like Python, the difference in performance is often negligible because the interpreter or compiler may optimize arithmetic operations like division or multiplication internally.
-            '''
-            counter.append(counter[i >> 1] + i % 2)
-        return counter
+            if offset * 2 == i:
+                offset = i
+            # Now we will add the no of 1's to ans
+            result[i] = 1 + result[i - offset]
+        
+        return result
