295faff83794134ac2c4cd6dd61226fac1451860
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2024-01-03 16:08:44 +1100

    Time: 73 ms (5.08%), Space: 17.2 MB (16.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,34 @@
-class Solution(object):
+class Solution:
+# This code snippet is a Python version of a dynamic programming solution to the
+# house robber problem, where you can't rob two adjacent houses. The `prev1` and `prev2`
+# variables keep track of the maximum profit from the previous houses, and the `num`
+# variable represents the current house's money.
     def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # Caculating the max sum of the 2 steps or 3 steps away from the current one, then get the max amount between the two
-        max3, max2, ad = 0, 0, 0
-        for cur in nums:
-            max3, max2, ad = \
-            max2, ad, max(max3 + cur, max2 + cur)
-        return max(max2, ad)
+        if not nums:
+            return 0
+        prev1 = 0
+        prev2 = 0
+        for num in nums:
+            temp = prev1
+            prev1 = max(prev2 + num, prev1)
+            prev2 = temp
+        return prev1
+"""
+def rob(nums):
+    if not nums:
+        return 0
+    prev1 = 0
+    prev2 = 0
+    for num in nums:
+        temp = prev1
+        prev1 = max(prev2 + num, prev1)
+        prev2 = temp
+    return prev1
+
+Cause runtime error, it seems like is becasue there is not self.
+In Python, the self keyword is used in object-oriented programming to refer to the instance of the current class. It allows access to the attributes and methods of the class in Python. When you create a new instance of a class, self refers to that specific instance.
+"""
+
+
+        
+        
