f04527eed1dbdbe79c45629ebe4726b4fa111b3e
Hannah Zhao <47459029+HannahYZhao@users.noreply.github.com>
2024-01-13 23:33:39 +1100

    Time: 45 ms (69.36%), Space: 17.3 MB (32.80%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,20 +1,32 @@
-class Solution(object):
-    def nextPermutation(self, nums):
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        i = j = len(nums) - 1
-        while i > 0 and nums[i - 1] >= nums[i]:
+        # To find next permutations, we'll start from the end
+        i = j = len(nums)-1
+        # First we'll find the first non-increasing element starting from the end
+        while i > 0 and nums[i-1] >= nums[i]:
             i -= 1
-        if i == 0: # Nums are in descending order
+        # After completion of the first loop, there will be two cases
+        # 1. Our i becomes zero (This will happen if the given array is sorted decreasingly). In this case, we'll simply reverse the sequence and will return 
+        if i == 0:
             nums.reverse()
-            return
-        k = i - 1 # Find the last "ascending" position
-        while nums[j] <= nums[k]:
+            return 
+        # 2. If it's not zero then we'll find the first number grater then nums[i-1] starting from end
+        while nums[j] <= nums[i-1]:
             j -= 1
-        nums[k], nums[j] = nums[j], nums[k]
-        l, r = k + 1, len(nums) -  1 # reverse the second part
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1; r -= 1
+        # Now out pointer is pointing at two different positions
+        # i. first non-assending number from end
+        # j. first number greater than nums[i-1]
+        
+        # We'll swap these two numbers
+        nums[i-1], nums[j] = nums[j], nums[i-1]
+        
+        # We'll reverse a sequence strating from i to end
+        nums[i:]= nums[len(nums)-1:i-1:-1]
+        # We don't need to return anything as we've modified nums in-place
+        '''
+            Dhruval
+        '''
+        
